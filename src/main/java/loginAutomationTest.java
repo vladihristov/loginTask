f3a13ba4945e519c5dd1da1d1c58681b61d2a370
vladihr <vladislav.poshta@gmail.com>
2023-06-11 10:34:43 +0300

    Login in Skillo 1st automation test

---
+++ src/main/java/loginAutomationTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loginAutomationTest {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\work\\Selenium\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://training.skillo-bg.com:4200/users/login");
+
+        WebElement username = driver.findElement(By.xpath("//input[@id='defaultLoginFormUsername']"));
+        username.sendKeys("vladislav.poshta@gmail.com");
+
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"defaultLoginFormPassword\"]"));
+        password.sendKeys("testPassword");
+
+        WebElement checkbox = driver.findElement(By.xpath("/html/body/app-root/div[2]/app-login/div/div/form/div/input"));
+        checkbox.click();
+
+        WebElement signIn = driver.findElement(By.xpath("//*[@id=\"sign-in-button\"]"));
+        signIn.click();
+    }
+}
